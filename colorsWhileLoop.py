b02bf9441e411c88047b9ce7163b7e16c28cf0f3
LMiranda3921 <143734360+LMiranda3921@users.noreply.github.com>
2023-09-14 11:33:00 -0400

    colorsWhileLoop.py
    creating a  while loop

---
+++ colorsWhileLoop.py
@@ -0,0 +1,4 @@
+colors = str(input("Would you like to know a color? Type y: "))
+while colors == "y" or colors == "Y":
+    print("red")
+    colors = str(input("Would you like to know another color? Type y: "))
