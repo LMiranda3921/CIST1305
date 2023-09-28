52e9545e222c54ba0af2543c3e10eb1a3f30d37c
LMiranda3921 <143734360+LMiranda3921@users.noreply.github.com>
2023-09-28 11:29:02 -0400

    Create PayRateProgram.py

---
+++ PayRateProgram.py
@@ -0,0 +1,4 @@
+hours  = float(input("Enter in the number of hours worked: "))
+payRate = float(input("Enter the hourly pay rate: "))
+grossPay = hours * payRate
+print("The gross pay is $", grossPay)
