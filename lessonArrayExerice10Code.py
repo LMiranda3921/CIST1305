5b71b6e85e218ee183f38f3bcf4903dc434b0756
LMiranda3921 <143734360+LMiranda3921@users.noreply.github.com>
2023-10-22 21:32:25 -0400

    Update lessonArrayExerice10Code.py

--- lessonArrayExerice10Code.py
+++ lessonArrayExerice10Code.py
@@ -1,8 +1,11 @@
 def main():
+    #ITs adding in the random function
+    #The repeat variable will call the arrayFunction to make more arrays to create a table 
     import random
     repeat = 0
     array = []
     saddlePoints = 0
+    totalPoints = 0
     
     def arrayFunction():
         NUMROW = 7
@@ -12,6 +15,8 @@ def main():
         row = 0
 
         while row < NUMROW:
+            #valeu is getting assigned a random number
+            #Then it is added onto the existing twoDArray
             value = random.randint(0,20)
             twoDArray.append(value)
             row = row + 1 
@@ -22,11 +27,13 @@ def main():
     def findPoints(array):
         print("saddle points!")    
     
-    while repeat < 7:    
+    while repeat < 7:
+        #calls the fuction that makes arrays multiple times
+        #it will then send that array to get checked for saddlepoints
         array = arrayFunction()
-        findPoints(array)
+        saddlePoints = findPoints(array)
+        print(saddlePoints)
         repeat = repeat + 1
         
-    
         
 main()
